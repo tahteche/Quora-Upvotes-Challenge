83607adf74fb875f8cbae1693ea401e34e4e44d6
Tah Teche <tahteche@gmail.com>
2018-07-30 19:04:09 -0600

    Make tests to run on individual outputs rather than group them

--- tests.py
+++ tests.py
@@ -2,32 +2,46 @@ import unittest, upvotes
 
 class TestUpvotes(unittest.TestCase):
 	def test_get_non_incr_subranges(self):
-		items = [1, 2, 3, 1, 1]
-		expected = [[], [[2, 3]], [[2, 4]]]
-		n = 5
+		items = [1, 2, 1, 4, 5, 7, 7, 8, 9, 0]
+		expected = [
+			[[1, 2]],
+			[[1, 2]],
+			[],
+			[],
+			[[5, 6]],
+			[[5, 6]],
+			[],
+			[[8, 9]]
+		]
+		n = 10
 		k = 3
-		result = []
 		output = []
-		for start in xrange(0, (n - k) + 1):
+		for start in xrange(0, n-k+1):
 			stop = start + k
 			output = upvotes.get_non_incr_subranges(start, stop, items,
 				output)
-			result.append(output)
-		self.assertEqual(result, expected)
+			self.assertEqual(output, expected[start])
 
 	def test_get_non_decr_subranges(self):
-		items = [1, 2, 3, 1, 1]
-		expected = [[[0, 2]], [[1, 2]], [[3, 4]]]
-		n = 5
+		items = [1, 2, 1, 4, 5, 7, 7, 8, 9, 0]
+		expected = [
+			[[0, 1]],
+			[[2, 3]],
+			[[2, 4]],
+			[[3, 5]],
+			[[4, 6]],
+			[[5, 7]],
+			[[6, 8]],
+			[[7, 8]]
+		]
+		n = 10
 		k = 3
-		result = []
 		output = []
-		for start in xrange(0, (n - k) + 1):
+		for start in xrange(0, n-k+1):
 			stop = start + k
 			output = upvotes.get_non_decr_subranges(start, stop, items,
 				output)
-			result.append(output)
-		self.assertEqual(result, expected)
+			self.assertEqual(output, expected[start])
 
 	def test_sum_subranges(self):
 		subranges = [[0, 2], [6, 10]]
